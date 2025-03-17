620b84a3dbdc78dc0a2ec538bb62229a94587c52
Vicho103 <vfulla@postgradouchile.cl>
2025-03-17 00:42:47 -0300

    Update app.js
    - Obtener el elemento de la lista
    - Limpiar la lista existente
    - Iterar sobre el arreglo
    - Agregar elementos a la lista

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-// Inicia declarando una variable de tipo array, que almacenará los nombres de los amigos ingresados. Ejemplo: let amigos = []
+// Inicia declarando una variable de tipo array, que almacenará los nombres de los amigos ingresados. 
 
 let nombreAmigo = [];
 console.log(nombreAmigo);
+
+//console.log(amigo);
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+    
+    //console.log(nombreAmigo);
+    
+    if (!nombreAmigo) {
+        alert(Por favor, inserte un nombre.);
+        return;
+     }
+     amigo.push(nombreAmigo);
+     console.log(amigo)
+     }
+ 
+
+
 
 
 
